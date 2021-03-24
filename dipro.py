e35305816ba8cd875373d7cefadc2d9179f04e81
Huidae Cho <grass4u@gmail.com>
2021-03-24 07:52:47 -0400

    Rename DIPPy (pypi package already exists) to DIPro

--- dipro.py
+++ dipro.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 ################################################################################
-# Name:    DIPPy: A Digital Image Processing Python module
+# Name:    DIPro: A Digital Image Processing Python module
 # Purpose: This module provides various digital image processing functions.
 # Author:  Huidae Cho
 # Since:   February 2, 2018
